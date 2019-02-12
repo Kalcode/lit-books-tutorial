f3ab5b4d29c23ff524af889c7839177d5170f3b7
David Clausen <davidclausen2051@gmail.com>
2019-02-12 11:19:07 -0600

    refactor folders and add section 13

---
+++ section-13-server-author-resolvers/server/src/graphql/typeDefs.js
@@ -0,0 +1,23 @@
+import { gql } from 'apollo-server-express';
+
+export const typeDefs = gql`
+  type Query {
+    users: [User]
+    user(_id: String!): User
+  }
+
+  type Mutation {
+    signup(username: String!, password: String!): String
+    login(username: String!, password: String!): String
+  }
+
+  type Author {
+    _id: String
+    name: String
+  }
+
+  type User {
+    _id: String
+    username: String
+  }
+`;
