1948c5f47656d07a1b8643041dad4d27eee0ffc8
David Clausen <davidclausen2051@gmail.com>
2019-02-26 19:10:05 -0600

    expand section 18 and 19

--- section-18-server-review-object-maps/server/src/graphql/object-maps/Book.js
+++ section-18-server-review-object-maps/server/src/graphql/object-maps/Book.js
@@ -1,7 +1,5 @@
 import { Author } from '../../db/models/Author';
 import { Book as BookModel } from '../../db/models/Book';
-import { Review } from '../../db/models/Review';
-import { Types } from 'mongoose';
 
 export const Book = {
   author: async function(obj) {
@@ -11,22 +9,5 @@ export const Book = {
     await BookModel.populate(obj, 'reviews');
 
     return obj.reviews;
-  },
-  rating: async function(obj) {
-    const ratings = await Review.aggregate([
-      {
-        '$match': {
-          'book': new Types.ObjectId(obj._id),
-        }
-      },
-      {
-        '$group': {
-          '_id': null,
-          'rating': { '$avg': { '$ifNull': ['$rating', 0] }}
-        }
-      },
-    ]);
-
-    return ratings[0] ? ratings[0].rating : 0;
   }
 };
